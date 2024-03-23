521f9bdb1ebe4e71ed254d0275bd461b59bec16c
muhammadhabibies <wtimorn@gmail.com>
2024-03-24 04:20:29 +0700

    refacto indentasi dan komentar yang tidak perlu di middleware

--- middlewares/role.js
+++ middlewares/role.js
@@ -1,20 +1,19 @@
-const role = (whoCanAccess) => {
-    return function (req, res, next) {
-        try {
-            // req.jwt ini dari file jwtAuth.js, yang berhasil verifikasi beserta berhasil menyimpan datanya
-            // console.log(req.jwt);
-            if (whoCanAccess.includes(req.jwt.role)) {
-                next();
-            } else {
-                throw { message: 'UNAUTHORIZED_ROLE' }
-            }
-        } catch (err) {
-            return res.status(401).json({
-                status: false,
-                message: 'UNAUTHORIZED_ROLE'
-            })
-        }
+const role = (user) => {
+  return function (req, res, next) {
+    try {
+      // console.log(req.jwt); // req.jwt ini dari middleware jwtAuth.js yang success
+      if (user.includes(req.jwt.role)) {
+        next();
+      } else {
+        throw { message: "UNAUTHORIZED_ROLE" };
+      }
+    } catch (err) {
+      return res.status(401).json({
+        status: false,
+        message: "UNAUTHORIZED_ROLE",
+      });
     }
-}
+  };
+};
 
 export default role;
