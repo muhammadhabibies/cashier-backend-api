8a2ccdb0ce8314d30efac2d649c3e698fd94fa70
muhammadhabibies <wtimorn@gmail.com>
2024-03-24 02:54:40 +0700

    hapus percobaan controller dsb

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,15 @@
-// import express
-import express from 'express';
+import express from "express";
 
-// import semua routes yang ada selain index.js ini
-import categories from './categories.js';
-import products from './products.js';
-import auth from './auth.js';
-import users from './users.js';
-import financial from './financial.js';
-import crud from './crud.js';
+import categories from "./categories.js";
+import products from "./products.js";
+import auth from "./auth.js";
+import users from "./users.js";
 
-// buat routernya lalu kemudian export
-var router = express.Router();
+const router = express.Router();
 
-router.use('/categories', categories);
-router.use('/products', products);
-router.use('/auth', auth);
-router.use('/users', users);
-router.use('/financial', financial);
-router.use('/crud', crud);
+router.use("/categories", categories);
+router.use("/products", products);
+router.use("/auth", auth);
+router.use("/users", users);
 
 export default router;
