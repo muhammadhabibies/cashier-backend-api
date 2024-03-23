8547adc1225f00106a965651a37fbf890033f850
muhammadhabibies <wtimorn@gmail.com>
2024-03-24 04:35:43 +0700

    refacto identasi dan komentar yang tidak perlu di libraries

--- libraries/isEmailExist.js
+++ libraries/isEmailExist.js
@@ -1,18 +1,16 @@
-import User from '../models/User.js'
+import User from "../models/User.js";
 
 const isEmailExist = async (email) => {
-    const user = await User.findOne({ email: email });
-    if (!user) { return false }
-    return true
-}
+  const user = await User.findOne({ email: email });
+  if (!user) return false;
+  return true;
+};
 
 const isEmailExistWithUserId = async (id, email) => {
-    const user = await User.findOne({ email: email, _id: { $ne: id } }); // ne ini adalah not equal. Jadi kalau email tidak sesuai dengan idnya maka baru tampilkan pesan error, tapi kalau id dan email sesuai maka tidak usah menampilkan pesan error email exist karena memang email ini sudah milik dia, jadi kita skip aja hal ini dan tidak menampilkan pesan error email exist lagi (ini itu untuk update user di file UserController.js)
-    if (!user) { return false }
-    return true
-}
+  const user = await User.findOne({ email: email, _id: { $ne: id } }); // $ne adalah not equal, untuk cek apakah email sesuai dengan id, ini itu untuk update di UserController
 
-// kalau cuma ada satu function, jadi langsung saja export default 
-// export default isEmailExist;
+  if (!user) return false;
+  return true;
+};
 
 export { isEmailExist, isEmailExistWithUserId };
